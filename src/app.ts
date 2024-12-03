9992823c2d14c53c81245d1cdbbba3b219f2adc5
Cristian Machuca <cmachuca32@gmail.com>
2024-12-03 21:32:58 +0200

    Setting dependencies.

---
+++ src/app.ts
@@ -0,0 +1,25 @@
+import "dotenv/config";
+import express from "express";
+import cors from "cors";
+import morgan from "morgan";
+
+const app = express();
+
+
+app.use(express.json());
+app.use(cors());
+app.use(morgan("dev"));
+
+
+
+
+
+
+const PORT = process.env.PORT || 4000;
+
+
+app.listen(PORT, () => {
+    process.env.NODE_ENV === "development"
+      ? console.log(`Server running at ${PORT} Development`)
+      : console.log(`Server running at ${PORT} Production`);
+  });
